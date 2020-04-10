c8f1dca900a0fb0a85cc9258c75c1f67e17cecc5
nicos <nicolas.turck@gmail.com>
2020-04-10 09:41:06 +0200

    add  exercise 2

--- exercice2/main.js
+++ exercice2/main.js
@@ -1,4 +1,11 @@
-var choices = 
-var choosenColor =
-
-for 
+// get elemment choices and choosencolor and register in variable
+var choices = document.getElementsByClassName("choice");
+var choosenColor = document.getElementById("choosenColor");
+/*start loop in all elem choice and register selected elem with click function and 
+return element and add it to var color and change syle to choosen color with color var */
+for (var iterator = 0; iterator < choices.length; iterator++) {
+  choices[iterator].onclick = function() {
+    var color = this.style.background;
+    choosenColor.style.background = color;
+  };
+}
