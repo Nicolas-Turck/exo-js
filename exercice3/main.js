f9875c948064e6d8133081b7bf7345f79953fcc8
nicos <nicolas.turck@gmail.com>
2020-04-10 10:06:36 +0200

    add  exercise 3

--- exercice3/main.js
+++ exercice3/main.js
@@ -1,13 +1,20 @@
-var input = 
-var addItem = 
-var shoppingList = 
+var input = document.getElementById("input");
+var addItem = document.getElementById("addItem");
+var shoppingList = document.getElementById("shoppingList");
 
-function addListItem
+function newproducts(course) {
+    // add to products 
+  var products = document.createElement("products");
+  products.innerHTML = course;
+    //add prodcts to shopp inglist
+  shoppingList.appendChild(products);
+}
 
-addItem.onclick = 
-
-  if
-
-input.onkeyup = 
-
-    if
+addItem.onclick = function() {
+    //function to save in var course the input after press button 
+  var course = input.value;
+    //verify lenght input if is not null save it
+  if(course.length > 0) {
+    newproducts(course);
+  }
+};
