2326dbbefb8d0ceb3034a840e01e8bafa667ff56
nicos <nicolas.turck@gmail.com>
2020-04-10 13:41:11 +0200

    add exe2  debutantmoyenhard work in progress

--- exoDebutantMoyenHard/exoDebutantMoyenHard.js
+++ exoDebutantMoyenHard/exoDebutantMoyenHard.js
@@ -6,3 +6,25 @@ var z = 15;
 var array = [w, x, y, z];
 var Maxnumber = Math.max.apply(Math, array);
 console.log(Maxnumber);
+
+//ecercices 2
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+  
+  readline.question(`how old are you ?`, (age) => {
+    function testNum(age) {
+        if (age < 0) {
+            console.log(`enter your realy age please!`)
+        }if (age >= 21) {
+            console.log(`access Ok!`)
+        }  else {
+            console.log(`you are ${age} Really!`)
+        }
+      }
+      
+    console.log(testNum(age));
+    //console.log(`enter your realy age please  ${age}!`)
+    readline.close()
+  })
