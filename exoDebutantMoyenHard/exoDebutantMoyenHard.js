9bda4eb55c3aed659c0987ad98684b94a34a56f4
nicos <nicolas.turck@gmail.com>
2020-04-11 09:17:57 +0200

    add exercices

--- exoDebutantMoyenHard/exoDebutantMoyenHard.js
+++ exoDebutantMoyenHard/exoDebutantMoyenHard.js
@@ -7,8 +7,8 @@ var array = [w, x, y, z];
 var Maxnumber = Math.max.apply(Math, array);
 console.log(Maxnumber);
 
-//ecercices 2
-const readline = require('readline').createInterface({
+//xcercices 2
+var readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
   })
@@ -30,4 +30,71 @@ const readline = require('readline').createInterface({
     console.log(testNum(age));
     
     readline.close()
+    
   })
+  
+// exercices 3
+  
+var hiddenNumber = 24;
+var readline2 = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+  })
+  readline2.question(`enter number ?`, (number) => {
+    function verify(number){
+      if (number < hiddenNumber) {
+          console.log(`hidden number is tallest!`)
+      }if (number > hiddenNumber) {
+          console.log(`hidden number is smallest!`)
+      }else {
+        console.log(`$$$$$youn found!$$$$$`)
+      }
+    }
+    console.log(verify(number));
+    readline2.close()
+
+  })
+
+// exercices 4
+var n = 0;
+while(n !=100);{
+    console.log(n);
+    n ++;
+}
+
+// exercices 5
+var n = 0;
+while(n !=100);{
+    console.log(n);
+    n = n+2;
+}
+  
+// exercices 6
+
+// exercices 7
+
+
+// exercices 8
+
+var n = 0;
+var symbol = "*"
+while (n != 6){
+    console.log(n*symbol)
+    n ++;
+}
+
+// exercices 9
+for (var i = 1; i <= 100; i++) {
+    if (i % 15 === 0) {
+      console.log("FIZZBUZZ");
+    } 
+    else if (i % 5 === 0) {
+      console.log("BUZZ");
+    } 
+    else if (i % 3 === 0) {
+      console.log("FIZZ");
+    } 
+    else {
+      console.log(i);
+    }
+}
